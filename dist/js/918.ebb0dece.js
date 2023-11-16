778df38d455e2c77171b68cbc909779a032c9c30
Kostiantyn Voskov <kvoskov@gmail.com>
2023-11-16 10:55:13 +0200

    adding dist

---
+++ dist/js/918.ebb0dece.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcourse_frontend"]=self["webpackChunkcourse_frontend"]||[]).push([[918],{5918:function(n,t,u){u.r(t),u.d(t,{default:function(){return l}});var e=function(){var n=this,t=n._self._c;return t("div",{staticClass:"404"},[n._v(" not found ")])},r=[],s={},c=s,f=u(1001),o=(0,f.Z)(c,e,r,!1,null,"3511406d",null),l=o.exports}}]);
+//# sourceMappingURL=918.ebb0dece.js.map
