e8f649761a1291f7ac80742a4793a98003fcf4fc
Kostiantyn Voskov <kvoskov@gmail.com>
2023-11-16 11:34:07 +0200

    deleted lesson object

--- src/mocks/mock_courses.js
+++ src/mocks/mock_courses.js
@@ -139,39 +139,4 @@ export const mockCourses = [
 			}
 		],
 	},
-	{
-		id: 1,
-		content: [
-			{
-				id: 1,
-				title: '<span class="md:text-2xl text-base font-bold">Front-end developer</span>',
-				subtitle: '<span class="opacity-40 text-base">5+ months * For beginners</span>',
-				description: '<span class="text-base">A Frontend developer is responsible for creating a convenient and aesthetic appearance of a web page on different devices, and also implements animations and other interface solutions.</span>',
-			},
-		],
-		lessons: [
-			{
-				id: 1,
-				title: '<span class="md:text-2xl text-base font-bold">Lesson 1 - Front-end developer</span>',
-				content: `
-				<p class="text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi scelerisque. Tortor pretium viverra suspendisse potenti nullam ac tortor. Lectus mauris ultrices eros in cursus turpis. Vestibulum sed arcu non odio euismod lacinia. Porta non pulvinar neque laoreet suspendisse interdum consectetur. Neque volutpat ac tincidunt vitae semper. Sit amet mauris commodo quis imperdiet massa tincidunt nunc. In metus vulputate eu scelerisque felis imperdiet proin. Tortor consequat id porta nibh. Turpis egestas maecenas pharetra convallis posuere morbi. Enim sit amet venenatis urna.</p>
-          
-          <p class="text-base mt-3">Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Pellentesque habitant morbi tristique senectus et netus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Volutpat maecenas volutpat blandit aliquam etiam erat velit. Eros donec ac odio tempor orci dapibus ultrices in. Tortor vitae purus faucibus ornare suspendisse. Nisl rhoncus mattis rhoncus urna neque viverra justo nec. Scelerisque viverra mauris in aliquam sem. Purus viverra accumsan in nisl nisi. Vitae ultricies leo integer malesuada nunc vel. Et tortor consequat id porta nibh. Dolor sit amet consectetur adipiscing elit. Odio ut sem nulla pharetra. Accumsan lacus vel facilisis volutpat est velit egestas dui id. Dignissim cras tincidunt lobortis feugiat vivamus at augue. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Pharetra et ultrices neque ornare aenean. Pellentesque elit eget gravida cum sociis natoque penatibus et.</p>
-          <p class="text-base mt-3">Quam pellentesque nec nam aliquam sem. Venenatis cras sed felis eget velit. Volutpat est velit egestas dui id ornare arcu. Diam quam nulla porttitor massa. Gravida arcu ac tortor dignissim convallis aenean et tortor at. Sem viverra aliquet eget sit amet tellus cras adipiscing enim. Pretium lectus quam id leo in vitae. Pellentesque dignissim enim sit amet venenatis. Congue eu consequat ac felis donec et odio. Massa tempor nec feugiat nisl pretium fusce. Curabitur vitae nunc sed velit dignissim sodales ut. Sit amet risus nullam eget felis eget nunc. Eget velit aliquet sagittis id consectetur.</p>
-          <p class="text-base mt-3">Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Facilisis volutpat est velit egestas dui. Enim diam vulputate ut pharetra sit amet aliquam id diam. Sagittis orci a scelerisque purus semper eget duis at. Phasellus faucibus scelerisque eleifend donec pretium. Pharetra convallis posuere morbi leo urna molestie at elementum eu. Purus gravida quis blandit turpis cursus in hac habitasse platea. At elementum eu facilisis sed odio. Dis parturient montes nascetur ridiculus mus mauris. Bibendum at varius vel pharetra vel turpis nunc. Feugiat pretium nibh ipsum consequat. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis.</p>`,
-				results: []
-			},
-			{
-				id: 2,
-				title: '<span class="md:text-2xl text-base font-bold">Lesson 2 - Front-end developer</span>',
-				content: `
-				<p class="text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi scelerisque. Tortor pretium viverra suspendisse potenti nullam ac tortor. Lectus mauris ultrices eros in cursus turpis. Vestibulum sed arcu non odio euismod lacinia. Porta non pulvinar neque laoreet suspendisse interdum consectetur. Neque volutpat ac tincidunt vitae semper. Sit amet mauris commodo quis imperdiet massa tincidunt nunc. In metus vulputate eu scelerisque felis imperdiet proin. Tortor consequat id porta nibh. Turpis egestas maecenas pharetra convallis posuere morbi. Enim sit amet venenatis urna.</p>
-          
-          <p class="text-base mt-3">Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Pellentesque habitant morbi tristique senectus et netus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Volutpat maecenas volutpat blandit aliquam etiam erat velit. Eros donec ac odio tempor orci dapibus ultrices in. Tortor vitae purus faucibus ornare suspendisse. Nisl rhoncus mattis rhoncus urna neque viverra justo nec. Scelerisque viverra mauris in aliquam sem. Purus viverra accumsan in nisl nisi. Vitae ultricies leo integer malesuada nunc vel. Et tortor consequat id porta nibh. Dolor sit amet consectetur adipiscing elit. Odio ut sem nulla pharetra. Accumsan lacus vel facilisis volutpat est velit egestas dui id. Dignissim cras tincidunt lobortis feugiat vivamus at augue. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Pharetra et ultrices neque ornare aenean. Pellentesque elit eget gravida cum sociis natoque penatibus et.</p>
-          <p class="text-base mt-3">Quam pellentesque nec nam aliquam sem. Venenatis cras sed felis eget velit. Volutpat est velit egestas dui id ornare arcu. Diam quam nulla porttitor massa. Gravida arcu ac tortor dignissim convallis aenean et tortor at. Sem viverra aliquet eget sit amet tellus cras adipiscing enim. Pretium lectus quam id leo in vitae. Pellentesque dignissim enim sit amet venenatis. Congue eu consequat ac felis donec et odio. Massa tempor nec feugiat nisl pretium fusce. Curabitur vitae nunc sed velit dignissim sodales ut. Sit amet risus nullam eget felis eget nunc. Eget velit aliquet sagittis id consectetur.</p>
-          <p class="text-base mt-3">Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Facilisis volutpat est velit egestas dui. Enim diam vulputate ut pharetra sit amet aliquam id diam. Sagittis orci a scelerisque purus semper eget duis at. Phasellus faucibus scelerisque eleifend donec pretium. Pharetra convallis posuere morbi leo urna molestie at elementum eu. Purus gravida quis blandit turpis cursus in hac habitasse platea. At elementum eu facilisis sed odio. Dis parturient montes nascetur ridiculus mus mauris. Bibendum at varius vel pharetra vel turpis nunc. Feugiat pretium nibh ipsum consequat. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis.</p>`,
-				results: []
-			}
-		],
-	}
 ]
