e0dcee0d3b25548529694b8654db64a5e06b1382
Kostiantyn Voskov <kvoskov@gmail.com>
2023-11-16 11:28:50 +0200

    adding rules

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <header v-if="showHeader" class="header relative md:py-6 bg-pink-400 md:p-0 p-4">
+    <header v-if="showHeader" class="header md:relative fixed w-full z-10 md:py-6 bg-pink-400 md:p-0 p-4">
       <Header class="md:block hidden" />
       <MobileHeader class="md:hidden block" />
     </header>
-    <div class="body">
+    <div class="body md:pt-0 pt-20">
       <div class="container-fluid mx-auto">
         <transition name="component-fade" mode="out-in">
           <router-view />
