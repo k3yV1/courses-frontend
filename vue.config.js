5f79ce039860125c4f6e8feb76a8bbdc83005de7
Kostiantyn Voskov <kvoskov@gmail.com>
2023-11-16 13:12:51 +0200

    change vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/course-frontend/',
+  publicPath: '/courses-frontend/',
   transpileDependencies: true
 })
