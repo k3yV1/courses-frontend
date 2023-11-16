01c3e48904307c84bebff25eb2cadc825095fa43
Kostiantyn Voskov <kvoskov@gmail.com>
2023-11-16 14:28:29 +0200

    Changed configuration files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/courses-frontend/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/courses-frontend/': '/',
   transpileDependencies: true
 })
